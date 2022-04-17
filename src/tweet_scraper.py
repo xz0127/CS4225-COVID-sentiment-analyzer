d4e655fbdfe090a62c99c22140b8c29a06131543
Xin Zhe <xinzhe0127@gmail.com>
2022-04-17 19:21:29 +0800

    Change SG query

--- src/tweet_scraper.py
+++ src/tweet_scraper.py
@@ -3,40 +3,70 @@ from pathlib import Path
 import pathlib
 import subprocess
 
+
 def scrapeTweet():
 
     # Global variables
     outputDir = "./../data/scraped_tweet"
 
-    isRawDataNeeded = True
+    isRawDataNeeded = False
     isRetweetNeeded = False
     isReplyNeeded = False
 
     keywords = [
-            "covid",
-            "corona",
-            "virus",
-            "Wuhan",
-            ]
+        "covid",
+        "corona",
+        "virus",
+        "Wuhan",
+        "Covid",
+        "Covid-19",
+        "Coronavirus",
+        "Mask",
+        "Quarantine",
+        "ICU",
+        "Vaccine",
+        "booster",
+        "lockdown"
+        "circuit-breaker"
+        "pandemic"
+    ]
 
+    keywordsSG = [
+        "(Singapore OR SG) AND "
+        "(covid",
+        "corona",
+        "virus",
+        "Wuhan",
+        "Covid",
+        "Covid-19",
+        "Coronavirus",
+        "Mask",
+        "Quarantine",
+        "ICU",
+        "Vaccine",
+        "booster",
+        "lockdown"
+        "circuit-breaker"
+        "pandemic)"
+    ]
 
     countries = [
         "US",
         "IN",
         "SG"
-        ]
+    ]
 
     lang = "en"
 
     startDate = date(2020, 2, 1)
     endDate = date(2020, 2, 2)
 
-    # Some initialization 
+    # Some initialization
     subprocess.call(["rm -rf " + outputDir], shell=True)
 
     # main scraping logic
     for country in countries:
-        
+
         outputPath = outputDir + "/" + country
         currDate = startDate
 
@@ -47,15 +77,22 @@ def scrapeTweet():
             commandBody = "snscrape --max-results 500 --jsonl twitter-search \""
 
             # append keywords filter to the command body
-            for i, keyword in enumerate(keywords):
-                if i > 0:
-                    commandBody += " OR "
-                commandBody += keyword
+            if country == "SG":
+                for i, keyword in enumerate(keywordsSG):
+                    if i > 0:
+                        commandBody += " OR "
+                    commandBody += keyword
+            else:
+                for i, keyword in enumerate(keywords):
+                    if i > 0:
+                        commandBody += " OR "
+                    commandBody += keyword
 
             # append location filter to the command body
-            commandBody += " near:" + country
-            if country == "SG":
-                commandBody += " within:15mi"
+            if country != "SG":
+                commandBody += " near:" + country
+            # if country == "SG":
+            #     commandBody += " within:30mi"
 
             # append language filter to the command body
             commandBody += " lang:" + lang
@@ -63,11 +100,11 @@ def scrapeTweet():
             # append retweet filter to the command body
             if not isRetweetNeeded:
                 commandBody += " -is:retweet"
-            
+
             # append reply filter to the command body
             if not isReplyNeeded:
                 commandBody += " -is:reply"
-            
+
             # generate and append date filter to the command body
             nextDate = currDate + timedelta(days=1)
             currDateStr = currDate.strftime('%Y-%m-%d')
@@ -75,8 +112,9 @@ def scrapeTweet():
             commandBody += " since:" + currDateStr + " until:" + nextDateStr
 
             # generate and append output file name to the command body
-            fileNameStr = "/" + country + "_" + currDateStr.replace("-","_") + ".json"
-            commandBody += "\" > " +  outputPath + fileNameStr
+            fileNameStr = "/" + country + "_" + \
+                currDateStr.replace("-", "_") + ".json"
+            commandBody += "\" > " + outputPath + fileNameStr
 
             # call command and move on to next iteration
             subprocess.call([commandBody], shell=True)
@@ -106,11 +144,11 @@ def scrapeTweet():
             # append retweet filter to the command body
             if not isRetweetNeeded:
                 commandBody += " -is:retweet"
-            
+
             # append reply filter to the command body
             if not isReplyNeeded:
                 commandBody += " -is:reply"
-            
+
             # generate and append date filter to the command body
             nextDate = currDate + timedelta(days=1)
             currDateStr = currDate.strftime('%Y-%m-%d')
@@ -118,10 +156,13 @@ def scrapeTweet():
             commandBody += " since:" + currDateStr + " until:" + nextDateStr
 
             # generate and append output file name to the command body
-            fileNameStr = "/Raw_" + currDateStr.replace("-","_") + ".json"
-            commandBody += "\" > " +  outputPath + fileNameStr
+            fileNameStr = "/Raw_" + currDateStr.replace("-", "_") + ".json"
+            commandBody += "\" > " + outputPath + fileNameStr
 
             # call command and move on to next iteration
             subprocess.call([commandBody], shell=True)
             currDate = nextDate
             print(commandBody)
+
+
+scrapeTweet()
